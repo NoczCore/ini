ee184bd2d54ef213d273e2b02ed846644de56be8
NoczCore <noczcore@gmail.com>
2015-10-16 15:43:46 +0200

    API

---
+++ elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","NoczCore\\Ini\\IniCollection"],["c","NoczCore\\Ini\\IniReader"],["c","NoczCore\\Ini\\IniReadingException"],["c","NoczCore\\Ini\\IniWriter"],["c","NoczCore\\Ini\\IniWritingException"]];
